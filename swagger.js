b1943f93514fd4076b7fbf679ec23990242538c4
thorlarsen <lar18033@byui.edu>
2022-05-13 19:03:22 -0500

    swagger autogen

---
+++ swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My Contacts API',
+    description: 'An API for managing contacts in CSE',
+  },
+  host: 'still-lowlands-88073.herokuapp.com',
+  schemes: ['https'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
