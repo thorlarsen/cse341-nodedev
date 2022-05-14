c31046095651e2fc7f6bc94ae64bc162305e07b0
thorlarsen <lar18033@byui.edu>
2022-05-14 14:46:25 -0500

    Small changes, release build

--- swagger.js
+++ swagger.js
@@ -12,8 +12,4 @@ const doc = {
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
 
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
-
 swaggerAutogen(outputFile, endpointsFiles, doc);
