153f6041afbb56d28d2068e45743ad374b66f6ef
thorlarsen <lar18033@byui.edu>
2022-05-13 20:02:54 -0500

    change response for '/'

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,18 @@
+const res = require('express/lib/response');
+
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-    res.redirect('/contacts');
-});
+/* routes.get('/', (req, res) => {
+    res.redirect('/api-docs');
+}); */
+
+routes.use('/', 
+    (gitHome = (req, res) => {
+        let gitHome = {
+        gitHomeURL: 'https://github.com/thorlarsen/cse341-nodedev',
+    };
+    res.send(gitHome);
+})
+);
 
 module.exports = routes;
