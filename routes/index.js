1402b0a4841a181a7a6da71fb85a6baa8a1db033
thorlarsen <lar18033@byui.edu>
2022-05-13 19:21:15 -0500

    swagger ui express

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 
 routes.use('/', require('./home'));
 routes.use('/contacts', require('./contacts'));
+routes.use('/swagger', require('./swagger'));
 
 module.exports = routes;
